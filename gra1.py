eb11c05639dacc10a4f271a8bb6a0917913aef0b
wojtekzbieg <wojtekzbieg56@gmail.com>
2025-07-22 13:32:50 +0200

    123

--- gra1.py
+++ gra1.py
@@ -3,17 +3,28 @@ import pygame
 clock = pygame.time.Clock()
 
 pygame.init()
+screen_width = 1600
+screen_height = 900
+screen = pygame.display.set_mode((screen_width, screen_height))
 running = True
-screen = pygame.display.set_mode((1640, 640))
-
 
 player = pygame.Rect((800, 320, 40, 40))
 
+background = (0, 0, 0)
+snake_colour = (255, 0, 0)
+cell_size = 10
+
+snake_position = [[int(screen_width / 2), int(screen_height / 2)]]
+
+def draw_screen():
+    screen.fill(background)
 
 while running:
 
     clock.tick(144)
-    screen.fill((0, 0, 0))
+
+    draw_screen()
+
     pygame.draw.rect(screen, (255, 0, 0), player)
 
     key = pygame.key.get_pressed()
